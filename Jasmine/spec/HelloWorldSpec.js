daa19fdb254417cd49ec334964152a3a61022a86
David Higgins <thehig@gmail.com>
2014-08-03 14:38:17 +0100

    Basic TDD Hello World

---
+++ Jasmine/spec/HelloWorldSpec.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Hig on 03/08/2014.
+ */
+describe("HelloWorld", function(){
+   it("should return hello world", function(){
+       expect(helloWorld()).toEqual("Hello World");
+   }) ;
+});
---
+++ Jasmine/src/HelloWorld.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Hig on 03/08/2014.
+ */
+
+var helloWorld = function(){
+    return "Hello World";
+};
